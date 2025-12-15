7b2b8b10fea2180adbce6266eca6044143110c8e
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-12-15 15:13:35 +0500

    Add Nav component with navigation links

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+function Nav() {
+    return (
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Menu</a>
+            </li>
+            <li>
+                <a href="#">Login</a>
+            </li>
+        </ul>
+    );
+}
+
+export default Nav;

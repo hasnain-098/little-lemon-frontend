65d2e8dae490973d87b11fe4bac50913ce792b4e
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-12-17 22:06:33 +0500

    feat: display confirmation message after table reservation

---
+++ src/components/ConfirmedBooking.jsx
@@ -0,0 +1,11 @@
+const ConfirmedBooking = () => {
+
+    return (
+        <>
+            <h1>Your table has been reserved successfully!</h1>
+            <p>We look forward to welcoming you at Little Lemon. See you soon!</p>
+        </>
+    );
+}
+
+export default ConfirmedBooking;

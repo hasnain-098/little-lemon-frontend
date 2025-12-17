5dc9116abc318e8850a702aea103cf51b82c55cf
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-12-18 02:41:02 +0500

    feat: import css file

--- src/components/ConfirmedBooking.jsx
+++ src/components/ConfirmedBooking.jsx
@@ -1,10 +1,12 @@
+import './ConfirmedBooking.css'
+
 const ConfirmedBooking = () => {
 
     return (
-        <>
+        <div className="confirmation-container">
             <h1>Your table has been reserved successfully!</h1>
             <p>We look forward to welcoming you at Little Lemon. See you soon!</p>
-        </>
+        </div>
     );
 }
 

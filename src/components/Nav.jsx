f138ad30a0893e4fa38b07d353ecdb64c2b8172c
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-12-18 02:45:01 +0500

    feat: import css file and replace Link with anchor tag

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import './Nav.css'
 
-const Nav = () => {
+const Nav = ({ menuOpen }) => {
     return (
-        <nav>
+        <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
             <ul style={{ listStyle: "none" }}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#login">Login</a></li>
             </ul>
         </nav>
     );

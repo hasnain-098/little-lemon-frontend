21094ab76877714ce01ddcda37493fa5e6531f6d
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-12-18 02:42:38 +0500

    feat: add div header-container and import css file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
-import logo from '../assets/logo.jpg'
+import './Header.css';
+import logo from '../assets/logo.jpg';
 import Nav from './Nav';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        <>
+        <div className='header-container'>
             <img src={logo} alt='Logo' />
-            <Nav />
-        </>
+            <button className='hamburger' onClick={toggleMenu} aria-label='Toggle Menu'>
+                ☰
+            </button>
+            <Nav menuOpen={menuOpen} />
+        </div>
     );
 }
 

bc463f60f46d693e7cd912326ce5ae00f54f0240
Hasnain Memon <hasnainmemon.018@gmail.com>
2025-12-18 02:40:26 +0500

    feat: import css file

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useFormik } from "formik";
+import './BookingForm.css'
 
 const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
 
